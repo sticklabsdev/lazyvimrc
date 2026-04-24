9fd0e84eebcef2951703025f6e472f57b8f795bc
Grissom <b.grissom@elatec.com>
2026-04-24 13:22:53 -0400

    Disable telescope picker (use snacks picker instead)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,14 +1,23 @@
 return {
-  "folke/snacks.nvim",
-  keys = {
-    {
-      "<leader>,",
-      function()
-        Snacks.picker.buffers({
-          formatters = { file = { filename_first = true } },
-        })
-      end,
-      desc = "Buffers",
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<leader>,",
+        function()
+          Snacks.picker.buffers({
+            formatters = { file = { filename_first = true } },
+          })
+        end,
+        desc = "Buffers",
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- Disable telescope's <leader>, binding so snacks can handle it
+      { "<leader>,", false },
     },
   },
 }
