199fb2e005a22ac8e9c48884d341bc1d20ebb20c
Grissom <b.grissom@elatec.com>
2026-06-19 16:38:43 -0400

    Change status line to black background

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      theme = {
+        normal = {
+          c = { bg = "#000000", fg = "#aaaaaa" },
+        },
+      },
+    },
+  },
+}
